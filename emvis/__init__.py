f2fdd74428a917444e9f1bbb4bf776726b65f806
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2019-12-02 13:37:29 +0100

    Updated version

--- emvis/__init__.py
+++ emvis/__init__.py
@@ -3,7 +3,7 @@
 This sub-module contains functions and classes that use emcore Python binding
 and provide utility functions to create views.
 """
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 from . import utils
 from . import models
